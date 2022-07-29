c2eab2150d76febb241c7d3684096e46d786562f
dcrisanto <dorelly.crisanto@gmail.com>
2022-07-28 19:18:48 -0500

    métodos del super prototipo Object

--- main.js
+++ main.js
@@ -10,19 +10,45 @@ console.log(`Accediendo a la propiedad name: ${Prueba.name}`);
 console.log('Accediendo al método speak:');
 console.log(Prueba.speak());
 
-const student = {
+const student1 = {
     name: 'Dorelly',
     email: 'dorelly.crisanto@gmail.com',
     age: 33
 }
 
 /*Llamando al método estático keys, getOwnPropertyNames del prototipo Object enviando cómo argumento a nuestro objeto nos devolverá un array con los keys de dicho objeto */
-console.log(Object.keys(student));
-console.log(Object.getOwnPropertyNames(student));
+console.log(Object.keys(student1));
+console.log(Object.getOwnPropertyNames(student1));
 /*entries nos devolverá un array de array donde cada array contiene el key y el valor del object */
-console.log(Object.entries(student));
+console.log(Object.entries(student1));
 /*devolverá un objeto con las propiedades de nuestro objeto, y cada propiedad tendrá como valor un objeto. Además en el valor devuelve los atriutos que contiene y así nos sirve para poder modificar las propiedas para así limitar el acceso, cómo sería usando polimorfismo*/
-console.log(Object.getOwnPropertyDescriptors(student));
+console.log(Object.getOwnPropertyDescriptors(student1));
 
+const studentPlatzi1 = {
+    name: 'Dorelly',
+    age: 33,
+    email: 'dorelly.crisanto@gmail.com',
+    approvedCourses: ['Curso 1'],
+    addCourses(newCourses) {
+        console.log(this);
+        console.log(this.approvedCourses);
+        this.approvedCourses.push(newCourses);
+    }
+}
+
+console.log(Object.keys(studentPlatzi1));
+console.log(Object.getOwnPropertyNames(studentPlatzi1));
+console.log(Object.entries(studentPlatzi1));
+console.log(Object.getOwnPropertyDescriptors(studentPlatzi1));
+
+/*Método estático del super prototipo Object para crear nuevas propiedades en nuestro objeto, recibe 3 parámetros: objeto, newAtributo, objeto de atributos del newAtributo*/
+Object.defineProperty(studentPlatzi1, "prueba", {
+    value: 'valor_prueba',
+    configurable: true,
+    enumerable: true,
+    writable: true
+});
+
+console.log(Object.getOwnPropertyDescriptors(studentPlatzi1));
 
 
